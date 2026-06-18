c8aa2f077abb762d2eda6bf409a2b51ce74104b2
PeiranZ <zhaopr0201@gmail.com>
2026-06-18 19:03:29 -0400

    Implement MedianFinder class for median calculation

---
+++ 295. Find Median from Data Stream./answer.cpp
@@ -0,0 +1,28 @@
+class MedianFinder {
+        priority_queue<int> left;
+        priority_queue<int, vector<int>, greater<>> right;
+public:
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        if(left.size() == right.size()){
+            right.push(num);
+            left.push(right.top());
+            right.pop();
+        }else{
+            left.push(num);
+            right.push(left.top());
+            left.pop();
+        }
+    }
+    
+    double findMedian() {
+        if(left.size() != right.size()){
+            return left.top();
+        }else{
+            return (left.top() + right.top()) / 2.0;
+        }
+    }
+};
