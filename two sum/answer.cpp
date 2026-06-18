b89df0a435ef99e039eaaf1a1bda90547c2b1b9b
PeiranZ <zhaopr0201@gmail.com>
2026-06-18 18:55:45 -0400

    Create answer.cpp

---
+++ two sum/answer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public: 
+vector<int> twosum(vector<int>& nums, int target){
+
+int n =sum.size();     // Fixed: nums.size() not sum.size()
+  for (int i=0;i<n,i++{    // Fixed: semicolon, not comma
+      for(int j=i+1; j<n. j++){     // Fixed: semicolon, not dot
+         if （nums[i]+nums[j] == target){   // Fixed: parentheses
+return{i,j}             / Fixed: semicolon
+          }
+        }
+      }
+ return{};
+}
+}
